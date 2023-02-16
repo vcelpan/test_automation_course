d004311cb80138ba719c54caae6929e3c16355c3
vcelpan <>
2023-02-16 10:53:03 +0200

    encapsulation task done.

---
+++ encapsulation/ContBancar.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class ContBancar {
+
+    private Integer numarCont;
+    private Float sold;
+
+
+    public Integer getNumarCont() {
+        return numarCont;
+    }
+
+    public void setNumarCont(Integer numarCont) {
+        this.numarCont = numarCont;
+    }
+
+    public Float getSold() {
+        return sold;
+    }
+
+    public void setSold(Float sold) {
+        this.sold = sold;
+    }
+}
